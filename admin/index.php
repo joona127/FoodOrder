4ec3dfc1ca6c0d0ed85799712733851f04cae611
joona127 <65039165+joona127@users.noreply.github.com>
2021-05-14 23:52:56 +0300

    alustus

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <div class="menu text-center">
+        <div class="wrapper">
+      <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Admin</a></li>
+      <li><a href="#">Category</a></li>
+      <li><a href="#">Food</a></li>
+      <li><a href="#">Order</a></li>
+      </ul>
+</div>
+</div>
+
+<div class="main-content">
+    <div class="wrapper">
+    Main content goes here
+</div>
+</div>
+
+<div class="footer">
+    <div class="wrapper">
+    <p class="text-center">2021 All rights reserves, Some Restaurant. Developed By - Joona Sorjonen</p>
+</div>
+</div>
+</body>
+</html>
